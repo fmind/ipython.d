de0d9a981e5169f6a9661c939a2c3b9891115fe0
Médéric Hurier <mederic.hurierz.partner@decathlon.com>
2021-08-17 07:49:19 +0000

    Changed editing mode to vi.

--- profile_default/ipython_config.py
+++ profile_default/ipython_config.py
@@ -569,7 +569,7 @@ c.InteractiveShellApp.extensions = ['autoreload']
 
 ## Shortcut style to use at the prompt. 'vi' or 'emacs'.
 #  Default: 'emacs'
-# c.TerminalInteractiveShell.editing_mode = 'emacs'
+c.TerminalInteractiveShell.editing_mode = 'vi'
 
 ## Set the editor used by IPython (default to $EDITOR/vi/notepad).
 #  Default: 'vi'
